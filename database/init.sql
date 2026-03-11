efda2ffe3bc658f61f9982ac2d6ebd45f2ff5cc4
luisantoniolopez771 <luisantoniolopez771@gmail.com>
2026-03-11 09:16:27 -0600

    Initial commit

---
+++ database/init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Piezas (
+    ID_Pieza NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Nombre VARCHAR2(50) NOT NULL,
+    Descripcion VARCHAR2(50),
+    Stock_Actual NUMBER(4) DEFAULT 0 CHECK (Stock_Actual >= 0),
+    Stock_Minimo NUMBER(4) DEFAULT 5
+);
+
+CREATE TABLE Usuarios (
+    ID_Usuario NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Nombre_Completo VARCHAR2(40) NOT NULL,
+    Rol VARCHAR2(20) NOT NULL,
+    Contrasena VARCHAR2(20)
+);
+
+CREATE TABLE Movimientos (
+    ID_Movimiento NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ID_Pieza NUMBER NOT NULL,
+    ID_Usuario NUMBER NOT NULL,
+    Tipo_Movimiento VARCHAR2(20) CHECK (Tipo_Movimiento IN ('ENTRADA', 'SALIDA')),
+    Cantidad NUMBER(4) NOT NULL CHECK (Cantidad > 0),
+    Fecha_Hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT ID_Pieza_Mov_Pie FOREIGN KEY (ID_Pieza) REFERENCES Piezas (ID_Pieza),
+    CONSTRAINT ID_Usuario_Mov_Usu FOREIGN KEY (ID_Usuario) REFERENCES Usuarios (ID_Usuario)
+);
