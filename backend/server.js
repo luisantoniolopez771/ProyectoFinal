7cf7cca2ce61d4e64c7a33061bd32b5d88e74bc8
luisantoniolopez771 <luisantoniolopez771@gmail.com>
2026-04-12 13:16:16 -0600

    BACKEND CONSULTA

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,12 @@ app.get('/api/test', async (req, res) => {
     let connection;
     try {
         connection = await oracledb.getConnection(dbConfig);
-        
+
         const result = await connection.execute(`SELECT table_name FROM user_tables`);
-        
-        res.json({ 
-            mensaje: "¡Conexión exitosa a Oracle!", 
-            tablas: result.rows 
+
+        res.json({
+            mensaje: "¡Conexión exitosa a Oracle!",
+            tablas: result.rows
         });
 
     } catch (err) {
@@ -35,6 +35,96 @@ app.get('/api/test', async (req, res) => {
                 await connection.close();
             } catch (err) {
                 console.error(err);
+            }
+        }
+    }
+});
+
+app.post('/api/login', async (req, res) => {
+    const usuarioRecibido = req.body.usuario;
+    const passwordRecibido = req.body.password;
+
+
+    let connection;
+
+    try {
+        connection = await oracledb.getConnection(dbConfig);
+
+        const consultaLogin = `SELECT * FROM Usuarios WHERE Nombre_Completo = :usr AND Contrasena = :pwd`;
+
+        const resultLogin = await connection.execute(
+            consultaLogin,
+            {
+                usr: usuarioRecibido,
+                pwd: passwordRecibido
+            },
+            {
+                outFormat: oracledb.OUT_FORMAT_OBJECT
+            }
+        );
+
+        if (resultLogin.rows.length > 0) {
+            const usuarioEncontrado = resultLogin.rows[0];
+
+            const rolEncontrado = usuarioEncontrado.ROL;
+
+            res.json({
+                exito: true,
+                mensaje: usuarioRecibido,
+                rol: rolEncontrado
+            });
+        } else {
+            res.status(401).json({
+                exito: false,
+                error: `Usuario o Contraseña incorrectos`
+            })
+        }
+    } catch (err) {
+        console.error("Error en el servidor: ", err);
+        res.status(500).json({ exito: false, error: "Fallo la conexión a la base de datos" });
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error("Error al cerrar la conexion: ", err);
+            }
+        }
+    }
+});
+
+
+app.get('/api/inventario', async (req, res) => {
+    let connection;
+
+    try {
+        connection = await oracledb.getConnection(dbConfig)
+
+        const consultaInventario = `SELECT Piezas.Nombre, Piezas.Marca, Piezas.Medida, Categorias.Nombre_Categoria AS Categoria, Ubicaciones.Anaquel, Piezas.Stock_Actual AS Stock FROM Piezas 
+        JOIN Categorias ON Piezas.ID_Categoria = Categorias.ID_Categoria
+        JOIN Ubicaciones ON Piezas.ID_Ubicacion = Ubicaciones.ID_Ubicacion`;
+
+        const resultado = await connection.execute(
+            consultaInventario,
+            [],
+            {
+                outFormat: oracledb.OUT_FORMAT_OBJECT
+            }
+        );
+            res.json({
+                exito: true,
+                datos: resultado.rows
+            });
+
+    } catch (err) {
+        console.error("Error consultando el inventario:", err);
+        res.status(500).json({ exito: false, error: "Fallo al traer los datos" });
+    } finally {
+        if (connection){
+            try {
+                await connection.close();
+            } catch (err){
+                console.error("Error al cerrar la conexion: ", err);
             }
         }
     }
